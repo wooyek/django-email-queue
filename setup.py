638819e46299cea4aa1d0e088bb338154b34d701
Janusz Skonieczny <git@wooyek.com>
2016-10-23 23:10:52 +0200

    Bump version: 0.9.10 → 0.9.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.9.10"
+version = "0.9.11"
 
 setup_kwargs = {
     'name': "django-email-queue",
