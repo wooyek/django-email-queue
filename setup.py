668314720ee96e3a997d3f7619dfb8ef3f448776
Janusz Skonieczny <git@wooyek.com>
2016-10-23 11:37:28 +0200

    Bump version: 0.9.2 → 0.9.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.9.2"
+version = "0.9.3"
 
 setup_kwargs = {
     'name': "django-email-queue-backend",
